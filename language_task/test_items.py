84c455a9a301044d18f7b73b958d27e688409501
Eleonora Belova <eleo.belova@yandex.ru>
2021-01-12 15:50:28 +0100

    test_items.py fix

--- language_task/test_items.py
+++ language_task/test_items.py
@@ -6,4 +6,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_guest_should_see_language_from_command_line(browser):
     browser.get(link)
     time.sleep(5)
-    browser.find_element_by_css_selector('button.btn-add-to-basket')
+    has_button = browser.find_element_by_css_selector('button.btn-add-to-basket')
+    assert has_button, 'кнопка добавления в корзину отсутствует'
